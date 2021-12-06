95d0a83e744caa65c834934a91408fce6474a7b1
Yassiremt <yassiremtioui@gmail.com>
2021-12-06 01:49:14 +0100

    log

--- src/routers/stats.js
+++ src/routers/stats.js
@@ -243,11 +243,13 @@ router.get("/adminstats", auth, async (req, res) => {
         },
         { $group: { _id: null, sum: { $sum: "$count" } } }
       ]);
+      console.log({ sumDays });
       const extractedSumDays = sumDays.length > 0 ? sumDays[0].sum : 0;
       const parsedFromDate = dayjs(fromDate);
       const parsedToDate = dayjs(toDate);
       const datesDifference = parsedToDate.diff(parsedFromDate, "days");
       const averageDaily = extractedSumDays / datesDifference;
+      console.log({ averageDaily });
 
       const percentageAllDailyDeliveries = await Order.aggregate([
         {
