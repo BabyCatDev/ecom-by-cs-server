a3132171fe0564d85a6deb0393c32f5d36a28242
Yassiremt <yassiremtioui@gmail.com>
2021-12-07 19:12:49 +0100

    repush

--- src/routers/stats.js
+++ src/routers/stats.js
@@ -225,7 +225,6 @@ router.get("/adminstats", auth, async (req, res) => {
       // sum the counts of period
       const fromDate = new Date(req.query.fromDate);
       const toDate = new Date(req.query.toDate);
-
       const sumDays = await Order.aggregate([
         {
           $match: {
