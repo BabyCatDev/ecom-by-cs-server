731f663e8a12fc724ca4f595abe5029a12bdbe55
Yassiremt <yassiremtioui@gmail.com>
2021-12-09 21:28:45 +0100

    added notifcations handling

--- src/routers/order.js
+++ src/routers/order.js
@@ -4,6 +4,8 @@ const Order = require("../models/order");
 const OrderDetail = require("../models/orderDetail");
 const auth = require("../middleware/auth");
 const dayjs = require("dayjs");
+const { Expo } = require("expo-server-sdk");
+let expo = new Expo();
 
 const router = new express.Router();
 
@@ -544,6 +546,52 @@ router.patch("/delivery/:id", auth, async (req, res) => {
           }
         }
       );
+
+      //find the seller
+      const seller = await User.findOne({ _id: seller });
+      //send notifs to his phones
+      let messages = [];
+      const body =
+        req.user.fullName +
+        (status === "Failed"
+          ? ` couldn't deliver`
+          : status === "Succeed"
+          ? ` delivered successfuly`
+          : ` no info`);
+
+      for (let pushToken of seller.notifPushTokens) {
+        if (!Expo.isExpoPushToken(pushToken)) {
+          console.error(
+            `Push token ${pushToken} is not a valid Expo push token`
+          );
+          continue;
+        }
+        messages.push({
+          to: pushToken,
+          sound: "default",
+          body
+        });
+      }
+      let chunks = expo.chunkPushNotifications(messages);
+      let tickets = [];
+      (async () => {
+        // Send the chunks to the Expo push notification service. There are
+        // different strategies you could use. A simple one is to send one chunk at a
+        // time, which nicely spreads the load out over time:
+        for (let chunk of chunks) {
+          try {
+            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            console.log(ticketChunk);
+            tickets.push(...ticketChunk);
+            // NOTE: If a ticket contains an error code in ticket.details.error, you
+            // must handle it appropriately. The error codes are listed in the Expo
+            // documentation:
+            // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      })();
       res.send(order);
     } catch (e) {
       res.status(400).send(e);
