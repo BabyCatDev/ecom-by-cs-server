40b89205b1bd2a72d5f66ea496b96448b3f4b8b2
Yassiremt <yassiremtioui@gmail.com>
2021-12-17 05:58:51 +0100

    Added averageDaily to delivery guy

--- src/routers/stats.js
+++ src/routers/stats.js
@@ -311,16 +311,10 @@ router.get("/adminstats", auth, async (req, res) => {
         toDate.getMonth(),
         toDate.getDate() - 1
       );
-      var totalSundays = 0;
 
-      for (var i = fromDate; i <= dayBefore; ) {
-        if (i.getDay() == 0) {
-          totalSundays++;
-        }
-        i.setTime(i.getTime() + 1000 * 60 * 60 * 24);
-      }
       const datesDifference =
-        parsedToDate.diff(parsedFromDate, "days") - totalSundays;
+        parsedToDate.diff(parsedFromDate, "days") -
+        getTotalSundays(fromDate, dayBefore);
 
       const averageDaily = extractedSumDays / (datesDifference || 1);
 
@@ -598,15 +592,55 @@ router.get("/admindeliverystats/:id", auth, async (req, res) => {
           ),
         0
       );
-      // const averageBasket = realizedIncome / totalOrders;
+      /////////////
+      const fromDate = new Date(req.query.fromDate);
+      const toDate = new Date(req.query.toDate);
+      const sumDays = await Order.aggregate([
+        {
+          $match: {
+            delivery: {
+              $eq: userId
+            },
+            createdAt: {
+              $gte: req.query.fromDate ? fromDate : today,
+              $lt: req.query.toDate ? toDate : tomorrow
+            },
+            postponed: {
+              $eq: false
+            }
+          }
+        },
+        {
+          $group: {
+            _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+            count: { $sum: 1 }
+          }
+        },
+        { $group: { _id: null, sum: { $sum: "$count" } } }
+      ]);
+      const extractedSumDays = sumDays.length > 0 ? sumDays[0].sum : 0;
+      const parsedFromDate = dayjs(fromDate);
+      const parsedToDate = dayjs(toDate);
 
+      const dayBefore = new Date(
+        toDate.getFullYear(),
+        toDate.getMonth(),
+        toDate.getDate() - 1
+      );
+
+      const datesDifference =
+        parsedToDate.diff(parsedFromDate, "days") -
+        getTotalSundays(fromDate, dayBefore);
+
+      const averageDaily = extractedSumDays / (datesDifference || 1);
       const stats = {
         totalOrders,
         failedOrders,
         succeedOrders,
         holdOrders,
         turnoverRealized,
-        failedTurnover
+        failedTurnover,
+        averageDaily
       };
       res.status(200).send(stats);
     } catch (e) {
@@ -1131,5 +1165,17 @@ router.get("/admincompanystats/:id", auth, async (req, res) => {
     res.status(403).send();
   }
 });
+
+const getTotalSundays = (fromDate, dayBefore) => {
+  var totalSundays = 0;
+
+  for (var i = fromDate; i <= dayBefore; ) {
+    if (i.getDay() == 0) {
+      totalSundays++;
+    }
+    i.setTime(i.getTime() + 1000 * 60 * 60 * 24);
+  }
+  return totalSundays;
+};
 
 module.exports = router;
